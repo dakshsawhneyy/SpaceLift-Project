12e59b7f44d9b0768ababed8b4666fd4131efa1a
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-05-24 11:28:14 +0530

    Create variable.tf

---
+++ Terraform/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+  default = "ami-0e35ddab05955cf57"
+}
+
+variable "volume_size" {
+  default = 8
+}
+
+variable "public_key" {
+  type = string
+  default = "/mnt/workspace/tf-key.pub"
+}
